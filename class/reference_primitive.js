e566cf321fac86675b37ef7203071a282e2c1a44
Jaan19867 <123554945+Jaan19867@users.noreply.github.com>
2023-05-29 17:43:43 +0530

    this is my second comment

---
+++ class/reference_primitive.js
@@ -0,0 +1,14 @@
+// iske baare me jaanna jaroori he 
+const person={
+
+    name:'Riz'
+};
+const secondPerson=person;
+person.name='Azad';
+console.log(secondPerson);
+// uper me ye as reference pass hua he
+let a=6;
+let b=a;
+a++;
+console.log(b);
+//so uper ke case me ye primitve data type he
