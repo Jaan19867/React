ef483d6d35dfa8d1a582ae2008ed11978c958709
Jaan19867 <123554945+Jaan19867@users.noreply.github.com>
2023-05-29 23:57:13 +0530

    this is commit till outputiing dynamically

--- First_Project/my-first-project/src/components/ExpenseItem.js
+++ First_Project/my-first-project/src/components/ExpenseItem.js
@@ -1,11 +1,19 @@
 import './ExpenseItem.css';
 function ExpenseItem(){
+    const expenseDate= new Date(2021,2,28);
+    const expenseTitle='Car Insurance';
+    const expenseAmount=294.56;
+
+
+
 return (
     <div className="expense-item">  
-        <div>March 28th 2021</div>
-<h2> Expense item!</h2>
+        {/* <div>March 28th 2021</div> */}
+        {/* <div>{expenseDate}</div> */}
+        <div>{expenseDate.toLocaleDateString()}</div>
+<h2>{expenseTitle}</h2>
 <div className="expense-item__description">Car Insurance</div>
-<div className='expense-item__price'>$294.98</div>
+<div className='expense-item__price'>${expenseAmount}</div>
 </div>
 );
 }
