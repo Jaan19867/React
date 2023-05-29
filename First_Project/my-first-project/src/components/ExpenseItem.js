826998d947cedaf991533a89a8e926cdfbf06968
Jaan19867 <123554945+Jaan19867@users.noreply.github.com>
2023-05-29 23:13:22 +0530

    this is commit till outputting dunamic data&workingwith expression

--- First_Project/my-first-project/src/components/ExpenseItem.js
+++ First_Project/my-first-project/src/components/ExpenseItem.js
@@ -1,7 +1,12 @@
+import './ExpenseItem.css';
 function ExpenseItem(){
-return 
+return (
+    <div className="expense-item">  
+        <div>March 28th 2021</div>
 <h2> Expense item!</h2>
-
-
+<div className="expense-item__description">Car Insurance</div>
+<div className='expense-item__price'>$294.98</div>
+</div>
+);
 }
-export default ExpenseItem;
+export default ExpenseItem
