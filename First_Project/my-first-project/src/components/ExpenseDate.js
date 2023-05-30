abece8d9d0c3d283d727c2f57ef30c0635e07b03
Jaan19867 <123554945+Jaan19867@users.noreply.github.com>
2023-05-30 10:22:32 +0530

    this till we split big componenet into smaller componenet

---
+++ First_Project/my-first-project/src/components/ExpenseDate.js
@@ -0,0 +1,20 @@
+import './ExpenseDate.css'
+
+function ExpenseDate(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-Us", { daay: "2-digit" });
+  const year = props.date.getFullYear();
+  return (
+    <div className='expense-date'>
+      {/* <div>March 28th 2021</div> */}
+      {/* <div>{expenseDate}</div> */}
+      {/* <div>{props.date.toLocaleDateString()}</div> */}
+      {/* <div>{props.date.toLocaleString("en-Us", { month: "long" })}</div> */}
+      <div className='expense-date__month'>{month}</div>
+      <div className='expense-date__year'>{day}</div>
+      <div className='expense-date__day'>{year}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
--- First_Project/my-first-project/src/components/ExpenseItem.js
+++ First_Project/my-first-project/src/components/ExpenseItem.js
@@ -1,19 +1,23 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
   // above things for props it should not store here but in app.js
   // after using props we can rid of these const
-//   const expenseDate = new Date(2021, 2, 28);
-//   const expenseTitle = "Car Insurance";
-//   const expenseAmount = 294.56;
+  //   const expenseDate = new Date(2021, 2, 28);
+  //   const expenseTitle = "Car Insurance";
+  //   const expenseAmount = 294.56;
+// below is just helper
 
   return (
     <div className="expense-item">
-      {/* <div>March 28th 2021</div> */}
-      {/* <div>{expenseDate}</div> */}
-      <div>{props.date.toLocaleDateString()}</div>
+  
       {/* // below is code if we write const wagerah but we are using props */}
       {/* <h2>{expenseTitle}</h2> */}
-      <h2>{props.title}</h2>
+      {/* <h2>{props.title}</h2> */}
+      {/* <ExpenseDate></ExpenseDate> */} 
+       {/* as in do tag ke beech gap nahi he to  */}
+
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
