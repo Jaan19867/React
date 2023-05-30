2e7013e8d5ad3913f0f8bf56c259ee9cf41c23db
Jaan19867 <123554945+Jaan19867@users.noreply.github.com>
2023-05-30 09:24:47 +0530

    this commit before adding js logic to ourproject

--- First_Project/my-first-project/src/components/ExpenseItem.js
+++ First_Project/my-first-project/src/components/ExpenseItem.js
@@ -1,20 +1,24 @@
-import './ExpenseItem.css';
-function ExpenseItem(){
-    const expenseDate= new Date(2021,2,28);
-    const expenseTitle='Car Insurance';
-    const expenseAmount=294.56;
+import "./ExpenseItem.css";
+function ExpenseItem(props) {
+  // above things for props it should not store here but in app.js
+  // after using props we can rid of these const
+//   const expenseDate = new Date(2021, 2, 28);
+//   const expenseTitle = "Car Insurance";
+//   const expenseAmount = 294.56;
 
-
-
-return (
-    <div className="expense-item">  
-        {/* <div>March 28th 2021</div> */}
-        {/* <div>{expenseDate}</div> */}
-        <div>{expenseDate.toLocaleDateString()}</div>
-<h2>{expenseTitle}</h2>
-<div className="expense-item__description">Car Insurance</div>
-<div className='expense-item__price'>${expenseAmount}</div>
-</div>
-);
+  return (
+    <div className="expense-item">
+      {/* <div>March 28th 2021</div> */}
+      {/* <div>{expenseDate}</div> */}
+      <div>{props.date.toLocaleDateString()}</div>
+      {/* // below is code if we write const wagerah but we are using props */}
+      {/* <h2>{expenseTitle}</h2> */}
+      <h2>{props.title}</h2>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+      </div>
+      <div className="expense-item__price">${props.amount}</div>
+    </div>
+  );
 }
-export default ExpenseItem
+export default ExpenseItem;
